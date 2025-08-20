6f6a15f75ab716f224c9dfad626b837658a575b4
3582-490 <beethovven@hotmail.com>
2025-08-21 01:57:07 +0300

    Create About.cpp

---
+++ About/About.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    struct Person {
+        string name;    
+        string birthPlace; 
+        string hobby;     
+        string favorites;  
+    };
+
+    Person myInfo;
+    myInfo.name = "3582-490";
+    myInfo.birthPlace = "Moscow";
+    myInfo.hobby = "Collecting stamps";
+    myInfo.favorites = "Fairuz and Beethoven";
+
+    cout << "Name: " << myInfo.name << endl;
+    cout << "Birth Place: " << myInfo.birthPlace << endl;
+    cout << "Hobby: " << myInfo.hobby << endl;
+    cout << "Favorites: " << myInfo.favorites << endl;
+
+    return 0;
+}
